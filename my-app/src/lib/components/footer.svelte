a6d7d09c15c8451c184e6132d81de07cc6a44195
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-12 23:11:23 +0000

    smaller socials

--- my-app/src/lib/components/footer.svelte
+++ my-app/src/lib/components/footer.svelte
@@ -1,10 +1,10 @@
 <footer>
     <nav>
         <ul class="hidden md:flex justify-end gap-6 fixed bottom-9 right-4">
-        <li><a href="https://www.youtube.com"><img src="/inst.svg" alt=""></a></li>
-        <li><a href="https://www.youtube.com"><img src="/yt.svg" alt=""></a></li>
-        <li><a href="https://www.youtube.com"><img src="/spt.png" alt=""></a></li>
-        <li><a href="https://sunnyam.bandcamp.com/"><img src="/bc.svg" alt=""></a></li>
+        <li><a href="https://www.youtube.com"><img class="w-[35px]" src="/inst.svg"  alt=""></a></li>
+        <li><a href="https://www.youtube.com"><img class="w-[35px]" src="/yt.svg" alt=""></a></li>
+        <li><a href="https://www.youtube.com"><img class="w-[35px]" src="/spt.png" alt=""></a></li>
+        <li><a href="https://sunnyam.bandcamp.com/"><img class="w-[35px]" src="/bc.svg" alt=""></a></li>
         </ul>
     </nav>
 </footer>
