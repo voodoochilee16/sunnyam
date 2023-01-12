1a7b059ec7b9629f537ce7472c98b25162e669b7
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-12 22:38:54 +0000

    button align

--- my-app/src/lib/components/drawer.svelte
+++ my-app/src/lib/components/drawer.svelte
@@ -9,8 +9,8 @@
     <div class="drawer-content">
       <!-- Page content here -->
         <Hero></Hero>
-        <label for="my-drawer" class="cursor-pointer z-50 mix-blend-difference drawer-button fixed top-4 right-4 lg:top-12 lg:right-6">
-            <img class="h-[60px] lg:h-[60px]" src="hamburger.svg" alt="more">
+        <label for="my-drawer" class="cursor-pointer z-50 mix-blend-difference drawer-button fixed top-4 right-4 lg:top-4 lg:right-2">
+            <img class="h-[60px] lg:h-[40px]" src="hamburger.svg" alt="more">
           </label>
     </div> 
     <div class="drawer-side">
