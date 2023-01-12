66e262573062b67ce05aa4d5d5e099ed54f5ad60
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-12 17:40:30 +0000

    .

--- my-app/src/lib/components/drawer.svelte
+++ my-app/src/lib/components/drawer.svelte
@@ -9,7 +9,7 @@
     <div class="drawer-content">
       <!-- Page content here -->
         <Hero></Hero>
-        <label for="my-drawer" class="cursor-pointer z-50 mix-blend-difference drawer-button fixed top-4 right-4 lg:top-12 lg:right-12">
+        <label for="my-drawer" class="cursor-pointer z-50 mix-blend-difference drawer-button fixed top-4 right-4 lg:top-12 lg:right-6">
             <img class="h-[60px] lg:h-[60px]" src="hamburger.svg" alt="more">
           </label>
     </div> 
