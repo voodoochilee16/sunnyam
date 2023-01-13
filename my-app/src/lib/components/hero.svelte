645f548f92ed4a49dfdca7aed1a0ec34627874c5
paulo <paulo.carvalhoferreira@protonmail.com>
2023-01-13 15:03:54 +0000

    pysch

--- my-app/src/lib/components/hero.svelte
+++ my-app/src/lib/components/hero.svelte
@@ -1,5 +1,42 @@
 <script>
   import Drawer from "./drawer.svelte";
+  import Hero from "$lib/components/hero.svelte";
+    import Nav from "$lib/components/nav.svelte";
+
+    let m = {x: 0, y: 0};
+
+function handleMouse(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+    const rect = event.currentTarget.getBoundingClientRect();
+    const anchorX = rect.left + rect.width / 2;
+    const anchorY = rect.top + rect.height / 2;
+
+    const angleDeg = angle(m.x, m.y, anchorX, anchorY);
+    console.log(angleDeg)
+}
+
+function angle(cx, cy, ex, ey){
+    const dy = ey-cy;
+    const dx = ex-cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg
+
+}
+function rotate (event) {
+      m.x = event.clientX;
+    m.y = event.clientY;
+    const rect = event.target.getBoundingClientRect();
+    const anchorX = rect.left + rect.width / 2;
+    const anchorY = rect.top + rect.height / 2;
+
+    const angleDeg = angle(m.x, m.y, anchorX, anchorY);
+    console.log(angleDeg)
+    const eye = event.currentTarget;
+    // eye.style.transform = `rotate(${90+angleDeg}deg)`;
+    eye.style.filter = `hue-rotate(${angleDeg}deg)`;
+}
 </script>
 
 <div class="flex justify-center items-center">
