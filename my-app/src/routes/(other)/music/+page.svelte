9018a3632ce4859b9212f799476da004596c89e8
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-17 23:52:40 +0000

    fucking logos

--- my-app/src/routes/(other)/music/+page.svelte
+++ my-app/src/routes/(other)/music/+page.svelte
@@ -1,12 +1,13 @@
 <script>
     import Nav from "$lib/components/nav.svelte";
   import Musicdrawer from "$lib/components/musicdrawer.svelte";
-  import Footer from "$lib/components/footer.svelte";
+  import Footerw from "$lib/components/footerw.svelte";
 </script>
 
 
 <body class="bg-borderorange">
     <Musicdrawer></Musicdrawer>
-    <Footer></Footer>
+    <Footerw></Footerw>
+    
     
 </body>
