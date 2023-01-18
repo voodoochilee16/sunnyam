317918a93dec76e4108594f14e5755d63b82b46f
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-18 17:27:46 +0000

    logos

--- my-app/src/lib/components/drawer.svelte
+++ my-app/src/lib/components/drawer.svelte
@@ -11,7 +11,7 @@
       <!-- Page content here -->
         <Hero></Hero>
         <label for="my-drawer" class="cursor-pointer z-50 mix-blend-difference drawer-button fixed top-4 right-4 lg:top-4 lg:right-2">
-            <img class="h-[60px]  lg:h-[50px] mr-1" src="hamburger.svg" alt="more">
+            <img class="h-[40px]  lg:h-[50px] mr-1" src="hamburger.svg" alt="more">
           </label>
     </div> 
     <div class="drawer-side">
