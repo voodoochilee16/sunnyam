35a8b9d31abc77f5a48739fc49001969adac51c2
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-01-18 18:40:00 +0000

    menu bird

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -6,7 +6,35 @@
   import Drawer from "$lib/components/drawer.svelte";
   import Whitestar from "$lib/components/whitestar.svelte";
 
-
+  let m = {x: 0, y: 0};
+    function handleMouse(event) {
+        m.x = event.clientX;
+        m.y = event.clientY;
+        const rect = event.currentTarget.getBoundingClientRect();
+        const anchorX = rect.left + rect.width / 2;
+        const anchorY = rect.top + rect.height / 2;
+        const angleDeg = angle(m.x, m.y, anchorX, anchorY);
+        console.log(angleDeg)
+    }
+    function angle(cx, cy, ex, ey){
+        const dy = ey-cy;
+        const dx = ex-cx;
+        const rad = Math.atan2(dy, dx);
+        const deg = rad * 180 / Math.PI;
+        return deg
+    }
+    function rotate (event) {
+          m.x = event.clientX;
+        m.y = event.clientY;
+        const rect = event.currentTarget.getBoundingClientRect();
+        const anchorX = rect.left + rect.width / 2;
+        const anchorY = rect.top + rect.height / 2;
+        const angleDeg = angle(m.x, m.y, anchorX, anchorY);
+        console.log(angleDeg)
+        const eye = event.currentTarget;
+        eye.style.transform = `rotate(${90+angleDeg}deg)`;
+        eye.style.filter = `hue-rotate(${angleDeg}deg)`;
+    }
 </script>
 
 
